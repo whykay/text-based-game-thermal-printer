bd97eec68f2c732aac01dda06bc49682d0114dd0
Vicky Twomey-Lee <whykay@gmail.com>
2020-04-10 00:33:11 +0100

    Removed old commented out code (that used other thermal printer python library), also slightly modified output of the printed text.

--- tbg-thermal-printer.py
+++ tbg-thermal-printer.py
@@ -3,7 +3,6 @@ import json, random, time
 from PIL import Image
 from thermalprinter import *
 
-#import adafruit_thermal_printer
 import board, busio, serial
 import RPi.GPIO as GPIO
 
@@ -30,64 +29,33 @@ class Game:
     }
     """
     num_of_games = 0
-    #game_number = random.randint(0, num_of_games-1)
     game_number = 0
 
 def print_welcome_message(printer):
     ''' Prints the welcome message when game starts'''
+    # The Dublin Maker logo at the top
     printer.image(Image.open("dm-bw.png"))
 
-    #printer.feed(2)
-    #printer.double_height = True
-    #printer.underline = adafruit_thermal_printer.UNDERLINE_THICK
-    #printer.print("Welcome to Vicky's print and play your own text-based adventure game")
-    #printer.double_height = False
-    #printer.underline = None
+    # Welcome message and instructions to start game
     printer.out("Welcome to Vicky's print and play your own text-based adventure game",
                 double_height = True,
                 underline = 2)
     printer.feed(2)
-
-    #printer.bold = True
-    #printer.print("Push a button to start playing.")
-    #printer.bold = False
     printer.out("Push a button to start playing.", bold=True)
-    #printer.feed(2)
-
-    printed = False
     print_button_choices_text()
 
 def print_endgame_message():
-    #printer.justify = adafruit_thermal_printer.JUSTIFY_CENTER
-    #printer.print("Made by Vicky Twomey-Lee")
     printer.out("Made by Vicky Twomey-Lee", justify="C")
-
-    #printer.double_width = True
-    #printer.print("Maker Advocate\n\n")
-    #printer.double_width = False
     printer.out("Maker Advocate", double_width=True)
-
-    #printer.bold = True
-    #printer.print("Dublin Maker Festival\nSummer 2021\nDublinMaker.ie\n\n")
-    #printer.bold = False
     printer.out("Dublin Maker Festival", bold=True, justify="C")
     printer.out("Summer 2021", bold=True, justify="C")
     printer.out("DublinMaker.ie", bold=True, justify="C")
-
-    #printer.print("Inspired by Der Choosatron found at Berlin Game Science Center")
-    #printer.justify = adafruit_thermal_printer.JUSTIFY_LEFT
     printer.feed(1)
     printer.out("Inspired by Der Choosatron found at Berlin Game Science Center", justify="L")
-
-    #time.sleep(0.2)
-    #printer.image(Image.open("dm-bw.png"))
     printer.feed(5)
 
 def print_game_text(game_choice):
     current_game = game_choice.games[game_choice.game_number].get("game_stage")[game_choice.stage]
-    #print(f"[print_game_text] current_game: {current_game}")
-    ##print(f'{current_game.get("button")[game_choice.button_pressed]}')
-
 
     try:
         if game_choice.button_pressed != 2:
@@ -105,35 +73,14 @@ def print_game_text(game_choice):
         print_button_choices_text()
 
 
-    """
-    print(current_game[a_game.stage]["button"])
-    print
-
-    try:
-        if game_choice.button_pressed != None:
-            button_choice_text = current_game[a_game.stage]["button"][game_choice.button_pressed]
-            printer.print(button_choice_text)
-    except:
-        pass
-
-    printer.print(current_game[a_game.stage]["text"])
-
-    if a_game.stage < 2:
-        print_button_choices_text()
-    """
-
 def print_button_choices_text():
     printer.out(" "*5 + "|" + " "*20 + "|")
     printer.out("[yellow/left]" + " "*7 + "[blue/right]")
     printer.out("|", justify="C")
     printer.out("|", justify="C")
     printer.out("[black/reset]", justify="C")
-    #printer.out("\t\t|\t\t\t|")
-    #printer.out("[yellow/left]\t\t[blue/right]")
-    #printer.out("\t\t\t|\t\t\t")
-    #printer.out("\t\t\t|\t\t\t")
-    #printer.out("\t\t [black/reset]\t\t")
     printer.feed(3)
+
 
 def pick_a_game(json_data):
     a_game = Game()
@@ -144,22 +91,15 @@ def pick_a_game(json_data):
     a_game.game_number = random.randint(0, a_game.num_of_games-1)
     return a_game
 
-GPIO.setmode(GPIO.BCM)
 
+# Setting up and where the code starts
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 games = {0:"Fun game 1", 1:"Fun game 2"}
 game_picked = False
-
-#ThermalPrinter = adafruit_thermal_printer.get_printer_class(2.69)
-#RX = board.RX
-#TX = board.TX
-
-#uart = serial.Serial("/dev/serial0", baudrate=19200, timeout=3000)
-#printer = ThermalPrinter(uart, auto_warm_up=False)
-#printer.warm_up()
 
 printer = ThermalPrinter(port="/dev/serial0")
 
@@ -179,14 +119,6 @@ with open("games.json", "r") as f:
 json_data = json.loads(data)
 
 try:
-    """
-    a_game = Game()
-    a_game.games = json_data
-    a_game.num_of_games = len(a_game.games)
-
-    # Set up to pick a random game
-    a_game.game_number = random.randint(0, a_game.num_of_games-1)
-    """
     a_game = pick_a_game(json_data)
 
     print(f"> a_game.num_of_games: {a_game.num_of_games}")
@@ -218,18 +150,13 @@ try:
                 print_welcome_message(printer)
             else:
                 print(">END OF GAME, THANKS FOR PLAYING")
-                #printer.double_height = True
                 printer.feed(1)
-                printer.out("Thanks for playing!", double_height=True)
-                printer.out("To reset game, hit the black button!", justify="C")
+                printer.out("Thanks for playing!", double_height=True, justify="C")
+                printer.out("To reset game,", justify="C")
+                printer.out("hit the black button!", justify="C")
                 printer.feed(2)
-                #printer.double_height = False
 
                 print_endgame_message()
             a_game = pick_a_game(json_data)
-
-
-
-            #print_welcome_message(printer)
 except:
     GPIO.cleanup()
